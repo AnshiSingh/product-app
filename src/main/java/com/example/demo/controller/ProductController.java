c7632bb545550f4c9d475f5d3b4d1c4b6dbc6795
Anshika Singh <58014942+AnshiSingh@users.noreply.github.com>
2022-03-11 02:57:45 +0530

    initial commit

---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Product;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class ProductController {
+
+    @GetMapping("/products")
+    public ResponseEntity<Product> createProduct(){
+       return ResponseEntity.ok(Product.builder().productId(UUID.randomUUID().toString()).productName("Pen").price(20).build());
+    }
+}
